738f01f203efb6e0c7466c7fc9e828280d39ebe4
Carsten Burstedde <uni@burstedde.de>
2025-10-27 09:24:15 +0100

    Verschoenere fourblink

--- beispiele/fourblink/fourblink.ino
+++ beispiele/fourblink/fourblink.ino
@@ -1,13 +1,17 @@
 
 #define LED_PIN LED_BUILTIN
 
+/* globale statische Variablen sind nur in dieser Programmdatei sichtbar */
 static unsigned int led_millis;
 
 void
 setup (void)
 {
+	/* Konsolenoutput aufsetzen */
 	Serial.begin (9600);
+	while (!Serial) ;
 
+	/* einmaliger Vorbereitungscode */
 	pinMode (LED_PIN, OUTPUT);
 	led_millis = millis ();
 }
@@ -15,10 +19,12 @@ setup (void)
 void
 loop (void)
 {
+	/* lokale statische Variablen behalten ihren Wert über mehrere Aufrufe */
 	static unsigned char step;
 	static const unsigned char numsteps = 4;
 	static const unsigned int duration[numsteps] = { 700, 20, 130, 150 };
 
+	/* konstante Variablen erlauben keine weitere Zuweisung */
 	const unsigned int led_diff = millis () - led_millis;
 	if (led_diff >= duration[step]) {
 		led_millis += duration[step];
