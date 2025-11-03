01ba5bd11bec10ac6f7ff05cb51c0cd41dba2474
Carsten Burstedde <uni@burstedde.de>
2025-11-03 14:13:43 +0100

    Beispiel zu for-Schleifen

---
+++ beispiele/forschleife/forschleife.ino
@@ -0,0 +1,127 @@
+
+/* Demo zu for-Schleifen.
+ *
+ * Die for-Schleife eignet sich besonders zum Hochzaehlen.
+ * Dieses Programm enthaelt eine Vielfalt an Beispielen.
+ * Am besten kopieren Sie es, loeschen, was Sie nicht interessiert,
+ * und experimentieren mit dem Rest durch spielerische Modifikation.
+ *
+ * Kommentiere koennen alle Zeichen enthalten, insbesondere *.
+ */
+
+/* diese Funktion sollte strenggenommen static sein */
+void
+run_forschleife (void) {
+
+        /* Zaehlvariablen ohne besondere Anforderungen
+           nennt man gerne i, j, k, l, das ist aber nicht vorgegeben */
+        /* Sie koennen in C Variablen nennen, wie sie wollen, solange diese
+           - mit einem Buchstaben oder Unterstrich _ beginnen
+           - alle folgenden Zeichen ein Buchstabe, eine Zahl oder _ sind.
+         */
+        int i;
+
+        /*************************************/
+        Serial.println ("Erstes for-Beispiel");
+        /*************************************/
+
+        for (i = 0; /* die erste der drei Anweisungen laeuft vor Beginn */
+             i < 7; /* die zweite wird vor jeder Iteration ausgewertet
+                       und beendet die Schleife, wenn sie false ist */
+             i = i + 1 /* die dritte Anweisung laeuft nach jeder Iteration,
+                          danach geht es weiter bei der zweiten Anweisung */)
+        { /* dies ist der Schleifenkoerper, den wir solange ausfuehren,
+             wie die zweite Bedingung im for-Konstrukt wahr ist */
+
+                /* wir geben eine feste Zeichenkette zum Serial Monitor */
+                Serial.print ("Anfang Schleifenkoerper mit Zaehler i = ");
+
+                /* Variable i zum Serial Monitor und einen Zeilenumbruch */
+                Serial.println (i);
+
+                /* Verzoegere eine Zehntelsekunde fuers Auge */
+                delay (100);
+        }
+
+        /* was ist der Wert von i nach Ende der Schleife? */
+        Serial.print ("Nach der Schleife ist i = ");
+        Serial.println (i);
+
+        /**************************************/
+        Serial.println ("Zweites for-Beispiel");
+        /**************************************/
+
+        /* Es ist erlaubt, Ausdrucke im for-Konstrukt wegzulassen */
+	for (; i < 4; ) {
+                /* da noch i >= 4 ist, wird dieser Block nicht ausgefuhrt */
+                Serial.println ("Dies darf nicht erscheinen");
+        }
+
+        /* Wir ueberspringen jetzt bestimmte Zaehler, alles ist erlaubt */
+        for (i = 3; i < 17; i = i + 5) {
+                Serial.print ("Anfang Schleifenkoerper mit Zaehler i = ");
+                Serial.println (i);
+                delay (100);
+        }
+        Serial.print ("Nach der Schleife ist i = ");
+        Serial.println (i);
+
+        /**************************************/
+        Serial.println ("Drittes for-Beispiel");
+        /**************************************/
+
+        /* Wir verwenden unsere eigene Kontroll-Logik */
+        /* Hier verwenden wir zwei reservierte Woerter in C:
+           'continue' springt direkt zur dritten Anweisung im for-Konstrukt.
+                      Danach geht es wie sonst auch weiter mit der
+                      Ueberpruefung der zweiten Bedingung.
+           'break' beendet die Schleife sofort und springt hinter den Koerper.
+         */
+        for (i = 6;; delay (100)) {
+                Serial.print ("Anfang Schleifenkoerper mit Zaehler i = ");
+                Serial.println (i);
+
+                if (i < 9) {
+                        i = i + 1; /* erhoehe um eins */
+                        continue; /* springt zur dritten Anweisung im for */
+                }
+                if (i < 14) {
+                        i = i + 3; /* erhoehe nun um drei */
+                        continue; /* springt zur dritten Anweisung im for */
+                }
+                i = i + 4; /* andernfalls erhoehe um vier */
+
+                /* eine Abbruchbedingung brauchen wir noch */
+                if (i >= 29) {
+                        break; /* springt sofort hinter den Schleifenkoerper */
+                        /* Interessant: wir eliminieren so das finale Delay */
+                }
+
+                /* wie immer geht es von hier zur dritten Anweisung im for */
+        }
+        Serial.print ("Nach der Schleife ist i = ");
+        Serial.println (i);
+}
+
+void
+setup (void)
+{
+        /* eingebaute LED aktivieren, sie ist zunaechst aus */
+        pinMode (LED_BUILTIN, OUTPUT);
+
+        /* initialisieren und warten auf den Serial Monitor */
+        Serial.begin (9600);
+	while (!Serial);
+
+        /* rufe unser eigentliches Programm auf */
+        run_forschleife ();
+
+        /* wir sind fertig, die Lampe geht an und bleibt dann an */
+        digitalWrite (LED_BUILTIN, HIGH);
+}
+
+void
+loop (void)
+{
+        /* in dieser Funktion machen wir gar nichts */
+}
