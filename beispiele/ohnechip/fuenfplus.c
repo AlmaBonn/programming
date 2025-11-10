80c9219e9dfb7cdfbf72582a42bba1565923d6a0
Carsten Burstedde <uni@burstedde.de>
2025-11-10 12:34:51 +0100

    Fortschritt plus 5

--- beispiele/ohnechip/fuenfplus.c
+++ beispiele/ohnechip/fuenfplus.c
@@ -1,13 +1,17 @@
 
-/* Berechne das Inkrement einer B-adischen Zahl. */
+/* Berechne das Inkrement einer B-adischen Zahl */
 
 #include <stdio.h> /* In dieser Datei ist printf definiert */
+#include <stdint.h> /* Unsigned-Typen fester Bitbreite */
 #include <stdlib.h> /* In dieser EXIT_SUCCESS */
 
+/* Breite der Eingabezahl in Stellen */
 #define L 4
+
+/* Wir rechnen im B-adischen System */
 #define B 5
 
-/* Wir erwarten Input der Laenge L und Output der Laenge L + 1 */
+/* Wir erwarten Input der Breite L und Output der Breite L + 1 */
 static void
 pluseins (const int input[], int output[])
 {
@@ -28,6 +32,20 @@ pluseins (const int input[], int output[])
     }
 }
 
+/* Wir erwarten Input mit parametrisierter Breite */
+static uint32_t
+hornerschema (const int input[], int breite)
+{
+    int i;
+    uint32_t horner = 0;
+
+    /* Wir verschenken eine Multiplikation mit 0 fuer einfachen Code */
+    for (i = 0; i < breite; ++i) {
+        horner = B * horner + input[i];
+    }
+    return horner;
+}
+
 static void
 run (void) {
 
@@ -37,9 +55,14 @@ run (void) {
     /* Vorsicht: noch undefiniert */
     int resultat[L + 1];
 
+    uint32_t wertalt, wertneu;
+
     /* Inkrementiere out-of-place */
     pluseins (zahl, resultat);
 
+    /* Verifiziere die Rechnung */
+    wertalt = hornerschema (zahl, L);
+    wertneu = hornerschema (resultat, L + 1);
 }
 
 int
