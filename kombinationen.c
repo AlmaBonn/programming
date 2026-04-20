017ab759c3d375b7f5789cc1048eeffc609b77c1
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2026-04-20 08:13:08 +0200

    Erste funktionierende Fakultaet

--- kombinationen.c
+++ kombinationen.c
@@ -2,10 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Eine lokale Funktion, um die Fakultaet zu berechnen */
+static int
+fakultaet (int n) {
+
+	int i;
+	int faku = 1;
+
+	for (i = 1; i <= n; ++i) {
+
+		faku = faku * i;
+
+	}
+
+	return faku;
+}
+
+/* Die main-Funktion kann zwei Argumente bearbeiten:
+ * Das erste (hier genannt argc) ist die Anzahl
+ *    Woerter auf der Kommandozeile (inklusive des Programmnamens)
+ * und das zweite ein Array mit genau diesen Woertern.
+ */
 int
 main (int argc, char *argv[]) {
 
 	int n = 0;
+	int faku = -1;
 
 	if (argc > 1) {
 
@@ -13,8 +35,9 @@ main (int argc, char *argv[]) {
 
 	}
 
-	printf ("Fakultät von %d ist y\n", n);
+	faku = fakultaet (n);
 
+	printf ("Fakultät von %d ist %d\n", n, faku);
 
 	return 0;
 }
